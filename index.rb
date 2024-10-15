78530de170e7ce290474f159735eeef150a82434
Danilo <dundeepessoal@gmail.com>
2024-10-15 11:39:39 -0300

    stopping when required arguments are not filled

--- index.rb
+++ index.rb
@@ -2,26 +2,32 @@ require 'optparse'
 require 'ostruct'
 require 'yaml'
 
-options = {
-  source_file: 'source.yml',
-  dest_file: 'dest'
-}
-OptionParser.new do |opt|
-  opt.on('-s', '--source-file SOURCEFILE', 'Nome do arquivo YML que servirá de base pro filesystem') { |o| options[:source_file] = o }
-  opt.on('-d', '--destination-file DESTFILE', 'Nome do diretório onde o sistema será criado') { |o| options[:dest_file] = o }
+options = {}
+
+parser = OptionParser.new do |opt|
+  opt.banner = 'Usage: jd_gen [options]'
+  opt.on('-s', '--source SOURCE', 'Nome do arquivo YML que servirá de base pro filesystem') { |o| options[:source] = o }
+  opt.on('-d', '--destination DEST', 'Nome do diretório onde o sistema será criado') { |o| options[:dest] = o }
 end
 
-data = YAML.load(File.read(options[:source_file]))
+parser.parse!
+
+if options[:source].nil? || options[:dest].nil?
+  puts parser.help
+  exit 1
+end
+
+data = YAML.load(File.read(options[:source]))
 
 data.keys.each do |upper|
-  `mkdir "#{options[:dest_file]}/#{upper}"`
+  `mkdir "#{options[:dest]}/#{upper}"`
   middle_values = data[upper]
   middle_values.keys.each do |middle|
-    `mkdir "#{options[:dest_file]}/#{upper}/#{middle}"`
+    `mkdir "#{options[:dest]}/#{upper}/#{middle}"`
     final_values = data[upper][middle]
     if final_values
       final_values.keys.each do |final|
-        `mkdir "#{options[:dest_file]}/#{upper}/#{middle}/#{final}"`
+        `mkdir "#{options[:dest]}/#{upper}/#{middle}/#{final}"`
       end
     end
   end
